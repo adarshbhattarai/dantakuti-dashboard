52da36bd192834005eee161902540f20e0f90d03
Rishab Banskota <rbanskot@gmu.edu>
2019-07-14 18:50:31 -0400

    Working on Registration still...

--- src/main/java/com/dantakuti/dashboard/document/User.java
+++ src/main/java/com/dantakuti/dashboard/document/User.java
@@ -7,16 +7,7 @@ package com.dantakuti.dashboard.document;
 public class User {
     private String email;
     private String password;
-//    private String fullName;
-//
-//
-//    public String getFullName() {
-//        return fullName;
-//    }
-//
-//    public void setFullName(String fullName) {
-//        this.fullName = fullName;
-//    }
+
 
     public String getEmail() {
         return email;
---
+++ src/main/java/com/dantakuti/dashboard/service/RegistrationService.java
@@ -0,0 +1,13 @@
+package com.dantakuti.dashboard.service;
+
+import javax.servlet.ServletException;
+
+import com.dantakuti.dashboard.document.RegisteredUser;
+/*/
+Registration interface
+implementation under impl dir
+ */
+public interface RegistrationService {
+
+    String register(RegisteredUser user) throws ServletException;
+}
